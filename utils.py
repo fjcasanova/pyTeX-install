d600cac43ff9e4d8699ac23a81cab625a503f3f1
fjcasanova <61351919+fjcasanova@users.noreply.github.com>
2026-05-02 15:13:35 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -109,14 +109,14 @@ def add2path() :
 def compil(chemin_fichier):
     dossier = os.path.dirname(chemin_fichier)
     nom_fichier = os.path.basename(chemin_fichier)
-    system = platform.system()
-    if shutil.which("latexmk"):
-        cmd = ["latexmk", "-pdf", "-interaction=nonstopmode", nom_fichier]
-    elif system == "Windows" :
-        if shutil.which("pdflatex") is  None :
-            add2path()
-        cmd = ["pdflatex", "-interaction=nonstopmode", nom_fichier]
-    subprocess.run(cmd,cwd=dossier, stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL, check=True)
+#    system = platform.system()
+#    if shutil.which("latexmk"):
+#        cmd = ["latexmk", "-pdf", "-interaction=nonstopmode", nom_fichier]
+#    elif system == "Windows" :
+#        if shutil.which("pdflatex") is  None :
+#            add2path()
+#        cmd = ["pdflatex", "-interaction=nonstopmode", nom_fichier]
+    subprocess.run("latexmk",cwd=dossier, stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL, check=True)
 
 from resolutions import*
 from derivation import*
